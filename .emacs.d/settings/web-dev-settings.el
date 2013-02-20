8606bff988f702a610da0f7acc4ce2007c682daa
Cristi Rusu <cristi@velomedia.com>
2013-02-20 17:46:04 +0200

    Set default PHP coding style to Wordpress

--- .emacs.d/settings/web-dev-settings.el
+++ .emacs.d/settings/web-dev-settings.el
@@ -6,31 +6,7 @@
 (add-to-list 'load-path "~/.emacs.d/php-mode")
 (require 'php-mode)
 
-;(load "~/.emacs.d/nxhtml/autostart.el")
-;; For faster running
-;;M-x nxhtmlmaint-start-byte-compilation
-;(require 'mmm-mode)
-;(setq mmm-global-mode 'maybe)
-;(mmm-add-mode-ext-class nil "\\.php3?\\'" 'html-php)
-;(mmm-add-classes
-; '((html-php
-;    :submode php-mode
-;    :front "<\\?\\(php\\)?"
-;    :back "\\?>")))
-;(autoload 'php-mode "php-mode" "PHP editing mode" t)
-;(add-to-list 'auto-mode-alist '("\\.php3?\\'" . sgml-html-mode))
-
-;; Mumamo is making emacs 23.3 freak out:
-;(when (and (equal emacs-major-version 24)
-;           (equal emacs-minor-version 2))
-;  (eval-after-load "bytecomp"
-;    '(add-to-list 'byte-compile-not-obsolete-vars
-;                  'font-lock-beginning-of-syntax-function))
-;  ;; tramp-compat.el clobbers this variable!
-;  (eval-after-load "tramp-compat"
-;    '(add-to-list 'byte-compile-not-obsolete-vars
-;                  'font-lock-beginning-of-syntax-function)))
-
-;(setq mumamo-chunk-coloring 0)
+(custom-set-variables 
+  '(php-mode-coding-style (quote wordpress)))
 
 (provide 'web-dev-settings)
